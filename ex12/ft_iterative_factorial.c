ec2dff205546cbdf43e3ff38d8700de40a271458
Victor Cesar Vaz <vcesar-v@c2r4p6.42sp.org.br>
2023-07-17 15:52:06 -0300

    test

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: vcesar-v <vcesar-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:04:57 by vcesar-v          #+#    #+#             */
-/*   Updated: 2023/07/11 14:21:59 by vcesar-v         ###   ########.fr       */
+/*   Updated: 2023/07/17 15:51:23 by vcesar-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int	ft_iterative_factorial(int nb)
 	return (nb);
 }
 
-/* #include <stdio.h>
+// #include <stdio.h>
 
-int	main(void)
-{
-	printf("%d", ft_iterative_factorial(6));
-}
- */
+// int	main(void)
+// {
+// 	printf("%d", ft_iterative_factorial(13));
+// }
+
